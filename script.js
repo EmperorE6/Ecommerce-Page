d62d3bd0fac82a55aa6330eac4546e73b6d112f0
EmperorE6 <nboy707@gmail.com>
2023-11-13 18:25:47 +0100

    newCommit

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ let cartBox=document.getElementById("cartOpen");
 let sideBar=document.getElementsByClassName("side_bar");
 let closeCart=document.getElementsByClassName("close");
 let addToCart=document.getElementsByClassName("add_to_cart");
+let cena=document.getElementsByClassName("prize");
+let total=document.getElementById("totalprice");
+let loginBtn=document.getElementById("login");
 
-let cena=document.getElementById("cena");
-
-let parsiranaCena=parseInt(cena.innerHTML);
-
-
-
+let TotalPrice=0;
+let CurrentPrice=0;
+let maxItems=0;
 const boxesArr = [...boxes];
 
 boxesArr.forEach(function(elem){
@@ -28,32 +28,166 @@ closeCart[0].addEventListener("click",function(e){
 sideBar[0].style.display="none";
 });
 
+
 const addToCartArray=[...addToCart];
 
 addToCartArray.forEach(function(edenAddCart){
-    edenAddCart.addEventListener("click",function(){
-
-        const dodadeno=document.createElement("div");
-        const removeBtn=document.createElement("button");
-        removeBtn.className="novoKopce";
-        dodadeno.className="novDiv"
-        removeBtn.textContent="Remove";
-        dodadeno.innerHTML=
-        `
-        <img src="icons/shirt.jpg">
-        <p>Price:${cena.innerHTML}</p>
+        function Dodavaj(event){
+            maxItems++;
+            let btn=event.currentTarget;
+            let btn_parent=btn.parentElement;
+           let img=btn_parent.children[1].innerHTML;
+           let description=btn_parent.children[2].innerHTML;
+           let pricestar=btn_parent.children[3];
+           let price=pricestar.children[0].innerHTML;
+           
+                   const dodadeno=document.createElement("div");
+                   const removeBtn=document.createElement("button");
+                   removeBtn.className="novoKopce";
+                   dodadeno.className="novDiv"
+                   removeBtn.textContent="Remove";
+                   
+                       dodadeno.innerHTML=`
+                        <div class="cartContent">
+                        <div class="cartImg">${img}</div>
+                        <div class="cartDesc">${description}</div>
+                        <div class="cartPrize">$${price}</div>
+                        </div>
+           
+                       `;
+                   sideBar[0].append(dodadeno);
+                   dodadeno.appendChild(removeBtn);
+           
         
-        `;
-        sideBar[0].append(dodadeno);
-        dodadeno.appendChild(removeBtn);
-
-        removeBtn.addEventListener("click",function(){
-            dodadeno.remove();
-        
-        })
-    })
+                   let parsiranaPrice=parseInt(price);
+                   if(maxItems<6){
+                    TotalPrice+=parsiranaPrice;
+                    total.innerHTML="Total: "+ TotalPrice+"$";
+                   }
+                   
+                    
+                   removeBtn.addEventListener("click",function(){
+                       maxItems--;
+                       dodadeno.remove();
+                       TotalPrice-=parsiranaPrice;
+                       total.innerHTML="Total: "+ TotalPrice+"$";
+                       
+                   });
+                   console.log(maxItems);
+                   if(maxItems >= 6 ){
+                    
+                    dodadeno.remove();
+                }
+                
+        }       
+       edenAddCart.addEventListener("click",Dodavaj);
+   
 });
-console.log(cena);
 
+
+
+function val() {
+    d = document.getElementById("slct").value;
+   if(d == "Spring"){
+    boxes[5].style.display="block";
+    boxes[4].style.display="block";
+    boxes[6].style.display="block";
+
+    boxes[1].style.display="none";
+    boxes[2].style.display="none";
+    boxes[3].style.display="none";
+    boxes[0].style.display="none";
+    boxes[7].style.display="none";
+    boxes[8].style.display="none";
+    boxes[9].style.display="none";
+    boxes[10].style.display="none";
+    boxes[11].style.display="none";
+  
+   }
+   else if(d == "Winter"){
+    boxes[1].style.display="block";
+    boxes[3].style.display="block";
+    boxes[7].style.display="block";
+
+    boxes[0].style.display="none";
+    boxes[2].style.display="none";
+    boxes[4].style.display="none";
+    boxes[5].style.display="none";
+    boxes[6].style.display="none";
+    boxes[8].style.display="none";
+    boxes[9].style.display="none";
+    boxes[10].style.display="none";
+    boxes[11].style.display="none";
+   }
+   else if(d == "Summer"){
+    boxes[0].style.display="block";
+    boxes[2].style.display="block";
+    boxes[4].style.display="block";
+    boxes[6].style.display="block";
+
+   
+    boxes[1].style.display="none";
+    boxes[3].style.display="none";
+    boxes[5].style.display="none";
+    boxes[7].style.display="none";
+    boxes[8].style.display="none";
+    boxes[9].style.display="none";
+    boxes[10].style.display="none";
+    boxes[11].style.display="none";
+   }
+   else if( d == "Autumn"){
+    boxes[8].style.display="block";
+    boxes[9].style.display="block";
+    boxes[11].style.display="block";
+   
+
+    boxes[0].style.display="none";
+    boxes[1].style.display="none";
+    boxes[2].style.display="none";
+    boxes[3].style.display="none";
+    boxes[4].style.display="none";
+    boxes[5].style.display="none";
+    boxes[6].style.display="none";
+    boxes[7].style.display="none";
+    boxes[10].style.display="none";
+   }else{
+    for(let i=0;i<boxes.length;i++){
+        boxes[i].style.display="block";
+    }
+   }
+};
+
+loginBtn.addEventListener("click",function(){
+    const loginWindow=document.createElement("div");
+    document.body.append(loginWindow);
+    loginWindow.className="loginProzor";
+    loginWindow.innerHTML=
+    `
+    
+<div class="title_login">Welcome</div>
+<div class="undertitle_login">Please login or create your account</div>
+<div class="vnes_login">
+    <input type="text" placeholder="Enter Username" required>
+    <input type="text" placeholder="Enter Password" required>
+</div>
+<div class="login_kopce"><button>Login</button></div>
+<div class="createAcc">Create Account</div>
+
+    `
+
+    
+    
+    window.addEventListener("click",function(e){
+        if(e.target == loginBtn){
+            loginWindow.style.display="block";
+        }else if(e.target != loginBtn && e.target !== loginWindow && e.target !== loginWindow.children[0] && e.target !== loginWindow.children[1] && e.target !==loginWindow.children[2]
+           && e.target !== loginWindow.children[2].children[0] && e.target !== loginWindow.children[2].children[1] && e.target !== loginWindow.children[3] 
+           && e.target !== loginWindow.children[3].children[0] && e.target !== loginWindow.children[4])
+        {
+            loginWindow.style.display="none";
+        }
+    })
+    
+})
 
 
